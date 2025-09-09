fa435369285117d79403f9e1284266d9978b0b24
Davijo <danvj93@gmail.com>
2025-09-09 19:24:27 +0200

    Fixture examples

--- fixtures/test_calculator.py
+++ fixtures/test_calculator.py
@@ -5,12 +5,23 @@ import pytest
 def numbers():
     return [1, 2, 3]
 
-class TestSum:
-    def test_sum_all(self, numbers):
-        assert sum(numbers) == 6
+# Don't do this
+""" numbers = [1, 2, 3]
+class TestSumBasic:
+    def test_sum_extra(self):
+        numbers.append(4)
+        assert sum(numbers) == 10
 
+    def test_sum_basic(self):
+        assert sum(numbers) == 6 """
+    
+
+class TestSumFixture:
     def test_sum_extra(self, numbers):
         assert sum(numbers + [4, 5]) == 15
+    
+    def test_sum_all(self, numbers):
+        assert sum(numbers) == 6
 
     def test_sum_empty(self):
         assert sum([]) == 0
